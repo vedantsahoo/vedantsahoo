4d8f0d1135d89abab1e116ca51a2f2eeb4450847
VEDANT_SAHU <vedantvibhusahu1234567@gmail.com>
2024-11-25 22:33:34 +0530

    update

--- form.js
+++ form.js
@@ -12,8 +12,32 @@ document.getElementById('register').addEventListener('submit', function (event)
     const errorMessage = document.getElementById('error-message');
     errorMessage.style.display = 'none';
   
-    if (!firstName || !lastName || !email || !password || !confirmPassword) {
-      errorMessage.textContent = 'Please fill in all the fields.';
+    if (!firstName) {
+      errorMessage.textContent = 'Please enter your First Name.';
+      errorMessage.style.display = 'block';
+      return;
+    }
+
+    if (!lastName) {
+      errorMessage.textContent = 'Please enter your Last Name.';
+      errorMessage.style.display = 'block';
+      return;
+    }
+
+    if (!email) {
+      errorMessage.textContent = "Please enter your E-mail & it must contain '@' sign.";
+      errorMessage.style.display = 'block';
+      return;
+    }
+
+    if (!password) {
+      errorMessage.textContent = 'Please enter your Password.';
+      errorMessage.style.display = 'block';
+      return;
+    }
+
+    if (!confirmPassword) {
+      errorMessage.textContent = 'Please enter your Password again.';
       errorMessage.style.display = 'block';
       return;
     }
